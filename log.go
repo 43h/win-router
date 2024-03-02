a9f4a896e2ede7e3712304af719280760310a942
43h <cw.clx@outlook.com>
2024-03-02 21:04:08 +0800

    修改日志，增加显示命令行

--- log.go
+++ log.go
@@ -4,12 +4,20 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
 )
 
+var logFileFlag = true
 var logHandle *os.File
 
 func initLog() bool {
+	if logFileFlag == false {
+		return true
+	}
+
+	delLog()
 	fileName := time.Now().Format("2006-01-02-15-04-05") + ".log"
 	logHandle, err := os.OpenFile(fileName, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
@@ -24,7 +32,32 @@ func initLog() bool {
 }
 
 func closeLog() {
-	if logHandle {
-		logHandle.Close()
+	if logHandle != nil {
+		err := logHandle.Close()
+		if err != nil {
+			log.Println("fail to close file, ", err)
+		}
+	}
+}
+
+func delLog() {
+	dir := "./"
+
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() && strings.HasSuffix(info.Name(), ".log") {
+			err := os.Remove(path)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		log.Println("fail to del log, ", err)
 	}
 }
