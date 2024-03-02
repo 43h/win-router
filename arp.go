d60ce30c386b009181f1922a28d9855205a8c31b
43h <cw.clx@outlook.com>
2024-03-02 21:04:38 +0800

    更新arp

--- arp.go
+++ arp.go
@@ -1,40 +1,14 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"log"
 	"net"
-	"os/exec"
 	"strings"
-	"time"
-
-	"github.com/mdlayher/arp"
 )
 
-type ARPItem struct {
-	ip  net.IP
-	mac net.HardwareAddr
-}
+var ARPTable map[string](net.HardwareAddr) = map[string](net.HardwareAddr){}
 
-var ARPTable map[string](ARPItem) = map[string](ARPItem){}
-
-func readARPTable() bool {
-	cmd := exec.Command("arp", "-a")
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
-	if err != nil {
-		fmt.Println("fail to read arp table, ", err)
-		return false
-	} else {
-		parseARPTable(out.String())
-	}
-	return true
-}
-
-func parseARPTable(output string) {
-	lines := strings.Split(output, "\n")
+func parseARP(line string) {
+	lines := strings.Split(line, "\n")
 	for _, line := range lines {
 		fields := strings.Fields(line)
 		if len(fields) == 3 {
@@ -42,61 +16,39 @@ func parseARPTable(output string) {
 			mac := fields[1]
 			_, ok := ARPTable[ip]
 			if !ok {
-				item := ARPItem{net.IP(ip), net.HardwareAddr(mac)}
-				ARPTable[ip] = item
+				ARPTable[net.IP(ip).To4().String()] = net.HardwareAddr(mac)
 			}
 		}
 	}
 }
 
-func getArpByIp(ip net.IP) net.HardwareAddr {
-	v, ok := ARPTable[string(ip)]
-	if ok {
-		if string(ip) == string(v.ip) {
-			return v.mac
-		}
+func addArp(ip string, mac string) {
+	_, ok := ARPTable[ip]
+	if !ok {
+		ARPTable[ip] = net.HardwareAddr(mac)
 	}
-	return nil
 }
 
-func request() {
-	// 设置网络接口
-	ifi, err := net.InterfaceByName("eth0")
-	if err != nil {
-		log.Fatal(err)
+func getArp(ip string) (net.HardwareAddr, bool) {
+	v, ok := ARPTable[ip]
+	if ok {
+		return v, true
 	}
+	return nil, false
+}
 
-	// 创建ARP客户端
-	client, err := arp.Dial(ifi)
-	if err != nil {
-		log.Fatal(err)
+func getArpWithSearch(ip string, name string) (net.HardwareAddr, bool) {
+	if v, ok := ARPTable[ip]; ok {
+		return v, true
+	} else {
+		go arpRequest(ip)
 	}
-	defer client.Close()
+	return nil, false
+}
 
-	// 设置目标IP地址
-	ip := net.IPv4(192, 168, 1, 1)
-
-	// 发送ARP请求
-	if err := client.Request(ip); err != nil {
-		log.Fatal(err)
-	}
-
-	// 等待ARP回复
-	for {
-		msg, _, err := client.Read()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// 如果收到的是ARP回复，并且源IP地址与目标IP地址相同，则打印源硬件地址
-		if msg.Operation == arp.OperationReply && msg.SenderIP.Equal(ip) {
-			log.Println("ARP reply from", msg.SenderHardwareAddr)
-			return
-		}
-
-		// 如果在5秒内没有收到ARP回复，则退出
-		if time.Since(msg.Time) > 5*time.Second {
-			log.Fatal("timeout")
-		}
+func arpRequest(ip string) {
+	mac, err := net.ParseMAC("f8:56:c3:12:1a:be")
+	if err == nil {
+		ARPTable[ip] = mac
 	}
 }
