4f352a7a7a34b3d6b310dcceee4aebfb733cf53f
43h <cw.clx@outlook.com>
2024-04-27 23:34:05 +0800

    更新版本到0.2.2

--- version.go
+++ version.go
@@ -1,8 +1,8 @@
 package main
 
 const productName = "win-router"
-const version = "0.2.1"
-const updatetime = "20240302"
+const version = "0.2.2"
+const updatetime = "20240427"
 
 func showVersion() string {
 	return productName + version + "---" + updatetime
