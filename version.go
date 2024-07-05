beff485768d172d28ecf5642464d3fadc1111df5
43h <cw.clx@outlook.com>
2024-07-05 23:36:36 +0800

    更新版本0.2.2

--- version.go
+++ version.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 const productName = "win-router"
-const version = "0.2.2"
-const updatetime = "20240427"
+const version = "0.2.3"
+const updatetime = "20240705"
 
 func showVersion() {
 	fmt.Println(productName + "\n" + version + "." + updatetime)
